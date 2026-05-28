eea26be442f641b365ca5efc01e34d984e586633
FSK-Games <f-s-k@web.de>
2026-05-28 22:15:47 +0200

    Change BLITZTIPPTABELLE to always be true

--- fetch-wm.js
+++ fetch-wm.js
@@ -129,8 +129,8 @@ async function fetchJSON(endpoint) {
     if (!fs.existsSync("data")) fs.mkdirSync("data");
 
     // 👇 Toggle ONLINE schreiben
-    const BLITZTIPPTABELLE = (process.env.BLITZTIPPTABELLE || "false") === "true";
-
+    // const BLITZTIPPTABELLE = (process.env.BLITZTIPPTABELLE || "false") === "true";
+    const BLITZTIPPTABELLE = "true";
     const outputMatches = {
       blitztipptabelle: BLITZTIPPTABELLE,
       ...matches
