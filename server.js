e5611b2a55562564fded276114a32c6047608c67
FSK-Games <f-s-k@web.de>
2026-06-10 16:58:35 +0200

    Implement initial data fetch on server startup
    Added initial data fetch on server start using exec.

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 
 require("./scheduler");
+const { exec } = require("child_process");
+
+// 🔥 SOFORT-FETCH BEI START
+console.log("🚀 Server startet – initialer Daten-Fetch...");
+exec("node fetch-wm.js", (err) => {
+  if (err) console.error("Initial Fetch Fehler:", err);
+});
 
 app.use("/data", express.static("data"));
 
